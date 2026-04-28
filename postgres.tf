b5851633ee92564e2e7d2aa93b7e7de91c7097ed
Moody Rahman <moududur921@gmail.com>
2026-04-28 16:03:51 -0400

    kill postgres deploy

--- postgres.tf
+++ postgres.tf
@@ -1,108 +1,94 @@
 
-resource "kubernetes_namespace_v1" "data" {
-  metadata {
-    name = "data"
-  }
-}
+# resource "kubernetes_namespace_v1" "data" {
+#   metadata {
+#     name = "data"
+#   }
+# }
 
-resource "random_password" "postgres" {
-  length  = 24
-  special = false
-}
+# resource "helm_release" "postgresql" {
+#   name      = "postgresql"
+#   namespace = "data"
 
-resource "kubernetes_secret_v1" "postgres" {
-  metadata {
-    name      = "postgres-credentials"
-    namespace = "data"
-  }
+#   repository = "oci://registry-1.docker.io/bitnamicharts"
+#   chart      = "postgresql"
 
-  data = {
-    postgres-password = random_password.postgres.result
-  }
-}
+#   set = [
+#     {
+#       name  = "auth.username"
+#       value = "moody"
+#     },
+#     {
+#       name  = "auth.database"
+#       value = "postgres"
+#     },
+#     {
+#       name  = "auth.postgresPassword"
+#       value = "postgres"
+#     },
+#     {
+#       name  = "auth.password"
+#       value = "postgres"
+#     },
+#     {
+#       name = "auth.enablePostgresUser"
+#       value = "true"
+#     },
+#     {
+#       name  = "primary.persistence.enabled"
+#       value = "true"
+#     },
+#     {
+#       name  = "primary.persistence.existingClaim"
+#       value = "postgres-pvc"
+#     }
+#   ]
+# }
 
-resource "helm_release" "postgresql" {
-  name      = "postgresql"
-  namespace = "data"
+# # nfs-subdir-provisioner is already set to be the default provisioner
 
-  repository = "oci://registry-1.docker.io/bitnamicharts"
-  chart      = "postgresql"
-
-  set = [
-    {
-      name  = "auth.username"
-      value = "postgres"
-    },
-    {
-      name  = "auth.database"
-      value = "postgres"
-    },
-    {
-      name  = "auth.postgresPassword"
-      value = "postgres"
-    },
-    {
-      name  = "primary.persistence.enabled"
-      value = "true"
-    },
-    {
-      name  = "primary.persistence.existingClaim"
-      value = "postgres-pvc"
-    }
-  ]
-}
-
-# nfs-subdir-provisioner is already set to be the default provisioner
-
-output "postgres_password" {
-  value     = random_password.postgres.result
-  sensitive = true # hides it from terminal output
-}
-
-
-resource "kubernetes_persistent_volume_claim_v1" "postgres_pvc" {
-  metadata {
-    name      = "postgres-pvc"
-    namespace = "data"
-  }
+# resource "kubernetes_persistent_volume_claim_v1" "postgres_pvc" {
+#   metadata {
+#     name      = "postgres-pvc"
+#     namespace = "data"
+#   }
 
 
 
-  spec {
-    access_modes       = ["ReadWriteMany"]
-    storage_class_name = "nfs"
-    volume_name        = kubernetes_persistent_volume_v1.nfs_pv_postgres.metadata[0].name
+#   spec {
+#     access_modes       = ["ReadWriteMany"]
+#     storage_class_name = "nfs"
+#     volume_name        = kubernetes_persistent_volume_v1.nfs_pv_postgres.metadata[0].name
 
-    resources {
-      requests = {
-        storage = "5Gi"
-      }
-    }
-  }
-}
+#     resources {
+#       requests = {
+#         storage = "5Gi"
+#       }
+#     }
+#   }
+# }
 
 
-resource "kubernetes_persistent_volume_v1" "nfs_pv_postgres" {
-  metadata {
-    name = "nfs-pv-postgres"
-  }
+# resource "kubernetes_persistent_volume_v1" "nfs_pv_postgres" {
+#   metadata {
+#     name = "nfs-pv-postgres"
+#   }
 
-  spec {
-    capacity = {
-      storage = "5Gi"
-    }
+#   spec {
+#     capacity = {
+#       storage = "5Gi"
+#     }
 
-    access_modes                     = ["ReadWriteMany"]
-    persistent_volume_reclaim_policy = "Retain"
-    storage_class_name               = "nfs"
-    mount_options                    = ["hard", "nfsvers=4.1"]
+#     access_modes                     = ["ReadWriteMany"]
+#     persistent_volume_reclaim_policy = "Delete"
+#     storage_class_name               = "nfs"
+#     mount_options                    = ["hard", "nfsvers=4.1"]
 
-    persistent_volume_source {
-      nfs {
-        server    = "nfs"
-        path      = "/srv/nfs/shared/postgres"
-        read_only = false
-      }
-    }
-  }
-}
+#     persistent_volume_source {
+#       nfs {
+#         server    = "nfs"
+#         path      = "/srv/nfs/shared/postgres"
+#         read_only = false
+#       }
+#     }
+#   }
+# }
