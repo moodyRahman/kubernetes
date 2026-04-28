b020a3cae6a06247411edeb73c9b39c25ac65e93
Moody Rahman <moududur921@gmail.com>
2026-04-28 16:04:00 -0400

    enable longhorn

---
+++ longhorn.tf
@@ -0,0 +1,49 @@
+
+
+resource "kubernetes_namespace_v1" "longhorn-system" {
+  metadata {
+    name = "longhorn-system"
+  }
+}
+
+
+resource "helm_release" "longhorn" {
+  depends_on = [ kubernetes_namespace_v1.longhorn-system ]
+  name       = "longhorn"
+  repository = "https://charts.longhorn.io"
+  chart      = "longhorn"
+  namespace  = "longhorn-system"
+}
+
+resource "kubernetes_ingress_v1" "longhorn" {
+  depends_on = [helm_release.longhorn]
+
+  metadata {
+    name      = "longhorn"
+    namespace = "longhorn-system"
+  }
+
+  spec {
+    ingress_class_name = "traefik"
+
+    rule {
+      host = "longhorn.k8s.moody"
+
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = "longhorn-frontend"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
