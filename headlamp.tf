fec6c92bdf94879eea5e110108ed83e95fd19659
Moody Rahman <moududur921@gmail.com>
2026-05-15 22:25:01 -0400

    rm headlamp and eso tf

--- headlamp.tf
+++ headlamp.tf
@@ -1,82 +1,82 @@
 
-resource "helm_release" "headlamp" {
-  name       = "headlamp"
-  repository = "https://kubernetes-sigs.github.io/headlamp/"
-  chart      = "headlamp"
-  namespace  = "kube-system"
+# resource "helm_release" "headlamp" {
+#   name       = "headlamp"
+#   repository = "https://kubernetes-sigs.github.io/headlamp/"
+#   chart      = "headlamp"
+#   namespace  = "kube-system"
 
-  set = [
-    {
-      name  = "serviceAccount.create"
-      value = "false"
-    },
-    {
-      name  = "serviceAccount.name"
-      value = "headlamp-admin"
-    },
-  ]
-}
-
-
-resource "kubernetes_service_account_v1" "headlamp_admin" {
-  depends_on = [helm_release.headlamp]
-
-  metadata {
-    name      = "headlamp-admin"
-    namespace = "kube-system"
-  }
-}
-
-resource "kubernetes_cluster_role_binding_v1" "headlamp_admin" {
-  depends_on = [kubernetes_service_account_v1.headlamp_admin]
-
-  metadata {
-    name = "headlamp-admin"
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = "headlamp-admin"
-    namespace = "kube-system"
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-}
+#   set = [
+#     {
+#       name  = "serviceAccount.create"
+#       value = "false"
+#     },
+#     {
+#       name  = "serviceAccount.name"
+#       value = "headlamp-admin"
+#     },
+#   ]
+# }
 
 
-resource "kubernetes_ingress_v1" "headlamp" {
-  depends_on = [helm_release.headlamp]
+# resource "kubernetes_service_account_v1" "headlamp_admin" {
+#   depends_on = [helm_release.headlamp]
 
-  metadata {
-    name      = "headlamp"
-    namespace = "kube-system"
-  }
+#   metadata {
+#     name      = "headlamp-admin"
+#     namespace = "kube-system"
+#   }
+# }
 
-  spec {
-    ingress_class_name = "traefik"
+# resource "kubernetes_cluster_role_binding_v1" "headlamp_admin" {
+#   depends_on = [kubernetes_service_account_v1.headlamp_admin]
 
-    rule {
-      host = "headlamp.k8s.moody"
+#   metadata {
+#     name = "headlamp-admin"
+#   }
 
-      http {
-        path {
-          path      = "/"
-          path_type = "Prefix"
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = "headlamp-admin"
+#     namespace = "kube-system"
+#   }
 
-          backend {
-            service {
-              name = "headlamp"
-              port {
-                number = 80
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
+# }
+
+
+# resource "kubernetes_ingress_v1" "headlamp" {
+#   depends_on = [helm_release.headlamp]
+
+#   metadata {
+#     name      = "headlamp"
+#     namespace = "kube-system"
+#   }
+
+#   spec {
+#     ingress_class_name = "traefik"
+
+#     rule {
+#       host = "headlamp.k8s.moody"
+
+#       http {
+#         path {
+#           path      = "/"
+#           path_type = "Prefix"
+
+#           backend {
+#             service {
+#               name = "headlamp"
+#               port {
+#                 number = 80
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
--- infisical.tf
+++ infisical.tf
@@ -1,14 +1,14 @@
 
-resource "kubernetes_namespace_v1" "infisical" {
-  metadata {
-    name = "infisical"
-  }
-}
+# resource "kubernetes_namespace_v1" "infisical" {
+#   metadata {
+#     name = "infisical"
+#   }
+# }
 
 
-resource "helm_release" "eso" {
-  name             = "eso"
-  create_namespace = true
-  repository       = "https://charts.external-secrets.io"
-  chart            = "external-secrets"
-}
+# resource "helm_release" "eso" {
+#   name             = "eso"
+#   create_namespace = true
+#   repository       = "https://charts.external-secrets.io"
+#   chart            = "external-secrets"
+# }
